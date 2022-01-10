5bfda674cedb1b552b7ce8d60eb99fefa23c1549
Пользователь <anyaluk4shova@eandex.ru>
2022-01-10 22:09:46 +0700

    master

---
+++ laba_kvakva/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 0;
+        int rez = 0;
+        char c;
+
+        Console.WriteLine("Введите первое и второе число, или выберите операцию: +, -, *, /");
+        c = Convert.ToChar(Console.ReadLine());
+        switch (c)
+        {
+            
+        }
+        Console.Write(rez);
+        Console.ReadKey();
+    }
+
+
+}
